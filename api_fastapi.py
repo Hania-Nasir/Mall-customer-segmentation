f35e5266b7416615dec28e1522b06c5bf46fda6f
Hania Nasir <hanianasir250809@gmail.com>
2025-10-19 04:09:50 -0700

    fixed mistakes

--- api_fastapi.py
+++ api_fastapi.py
@@ -3,20 +3,24 @@ from pydantic import BaseModel
 import pandas as pd
 import joblib
 
-app=FastAPI(title="Mall Customer Segmentation API")
+app = FastAPI(title="Mall Customer Segmentation API")
 
-model=joblib.load("mall_customer_segmentation.joblib")
+# Load the trained model
+model = joblib.load("mall_customer_segmentation.joblib")
 
+# Input schema
 class CustomerInput(BaseModel):
-    annual_income:float
-    spending_score:float
+    gender: int        
+    annual_income: float
+    spending_score: float
 
 @app.get("/")
 def root():
-    return{"message : welcome to mall customer segmentation API"}
+    return {"message": "Welcome to Mall Customer Segmentation API"}
 
-@app.post("/Predict")
-def get_cluster(annual_income:float,spending_score:float):
-    X=pd.DataFrame([[annual_income, spending_score]], columns=["Annual Income (k$)", "Spending Score (1-100)"])
-    cluster =model.predict(X)[0]
-    return{"cluster":int(cluster)}
+@app.post("/predict")
+def get_cluster(input: CustomerInput):
+    X = pd.DataFrame([[input.gender, input.annual_income, input.spending_score]],columns=["Gender", "Annual Income (k$)", "Spending Score (1-100)"])
+
+    cluster = model.predict(X)[0]
+    return {"cluster": int(cluster)}
